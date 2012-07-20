243cc079e65a15f03a678d969077f28f2e5ed51e
Yuri Albuquerque <yuridenommus@gmail.com>
2012-07-20 15:45:04 -0400

    Adding three meteors to the game.

--- main_game.rb
+++ main_game.rb
@@ -1,22 +1,31 @@
 #!/usr/bin/env ruby
 
 require 'gosu'
-require './player.rb'
-require './lib/math/vector.rb'
+%w(player meteor).each { |s| require "./#{s}.rb" }
 
 class GameWindow < Gosu::Window
   def initialize
     super 800, 600, false
     self.caption = "Meteorocks"
     @player = Player.new(self, 400, 300)
+    @meteors = Array.new
+    3.times do
+      @meteors << Meteor.new(self)
+    end
   end
 
   def update
     @player.update
+    @meteors.each do |m|
+      m.update
+    end
   end
 
   def draw
     @player.draw
+    @meteors.each do |m|
+      m.draw
+    end
   end
 end
 
---
+++ meteor.rb
@@ -0,0 +1,24 @@
+%w(vector circle).each { |s| require "./lib/math/#{s}.rb" }
+
+class Meteor
+  def initialize(window)
+    @wnd = window
+    @circle = Circle.new(Vector.new(rand(800), rand(600)), 20)
+    angle = rand(Math::PI*2)
+    @vel = Vector.new(Math::cos(angle)*5, Math::sin(angle)*5)
+    @image = Gosu::Image.new(window, "content/sprites/meteor.bmp", false)
+    @angle = rand(Math::PI*2)
+    @ang_vel = rand(Math::PI*2)
+  end
+
+  def update
+    @circle.position += @vel
+    @circle.position.x %= 800
+    @circle.position.y %= 600
+    @angle += @ang_vel
+  end
+
+  def draw
+    @image.draw_rot(@circle.position.x, @circle.position.y, 1, @angle)
+  end
+end
